4d0aed3b5498c1c329241d15aec0731b098e03ce
wobblewobble <mcglynn.joe@gmail.com>
2016-02-23 13:48:12 +0000

    Zip with Powershell

--- Zip With Poweshell.ps1
+++ Zip With Poweshell.ps1
@@ -1,4 +1,7 @@
 # Zip a file in Powershell
+# reference Ed - Scripting Guy
+# https://blogs.technet.microsoft.com/heyscriptingguy/2015/03/09/use-powershell-to-create-zip-archive-of-folder/
+
 $Source = "E:\Reporting\SCOMReports" # Source Folder
 $Destination ="E:\Reporting\ZippedReports\SCOMFiles.zip" # Destination folder
 Add-Type -assembly "system.io.compression.filesystem"
